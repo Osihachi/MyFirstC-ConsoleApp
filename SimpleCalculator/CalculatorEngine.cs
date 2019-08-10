abaa8fbbe85c9fe422590c4fccb2b9a91ef270bb
Osihachi <50714555+Osihachi@users.noreply.github.com>
2019-08-10 06:26:45 +0100

    Add files via upload

---
+++ SimpleCalculator/CalculatorEngine.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+     public class CalculatorEngine
+    {
+        public double Calculate(string argOperation, double argFirstNumber, double argSecondNumber)
+        {
+            double results;
+
+            switch (argOperation.ToLower())
+            {
+                case "add":
+                case "plus":
+                case  "+":
+                    results = argFirstNumber + argSecondNumber;
+                    break;
+                case "subtract":
+                case "minus":
+                case  "-":
+                    results = argFirstNumber - argSecondNumber;
+                    break;
+                case "multiply":
+                case "times":
+                case "*":
+                    results = argFirstNumber * argSecondNumber;
+                    break;
+                case "divide":
+                case "divided by":
+                case "/":
+                    results = argFirstNumber / argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Specified Operation not Recognized");
+                }
+
+            return results;
+        }
+    }
+}
---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+              {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
+
+                double FirstNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                double SecondNumber = inputConverter.ConvertInputToNumeric(Console.ReadLine());
+                string operation = Console.ReadLine();
+
+                double results = calculatorEngine.Calculate(operation, FirstNumber, SecondNumber);
+
+
+                Console.WriteLine(results);
+               }
+            catch (Exception ex)
+                {
+                //1000: start logging
+                Console.WriteLine( ex.Message);
+                }
+           
+        }
+    }
+}
